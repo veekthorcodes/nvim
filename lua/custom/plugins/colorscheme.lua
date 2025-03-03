5733c04825883cd771b90383384e853915741df1
veekthorcodes <amadi490@yahoo.com>
2025-03-03 21:32:50 +0100

    changed theme to tokyo night dark

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,10 +1,10 @@
 return {
-  "folke/tokyonight.nvim",
+  'folke/tokyonight.nvim',
   lazy = false,
   priority = 1000,
   opts = {},
   init = function()
-    vim.cmd.colorscheme 'tokyonight-day'
+    vim.cmd.colorscheme 'tokyonight'
     vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
     vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
   end,
